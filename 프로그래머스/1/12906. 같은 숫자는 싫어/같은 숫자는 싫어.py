d9f78226fb507fc804261677366523b800d9648c
Seungoh Jung <147333637+jungseungoh@users.noreply.github.com>
2024-09-05 15:39:19 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 64.69 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,9 +1,9 @@
-def solution(arr): 
-    answer = [arr[0]]
-    for i in range(1, len(arr)):
-        if arr[i] != arr[i-1]:
-            answer.append(arr[i])
-    return answer
+def solution(arr):
+    stack = []
+    for x in arr:
+        if not stack or stack[-1] != x:
+            stack.append(x) 
+    return stack
 
 
 
